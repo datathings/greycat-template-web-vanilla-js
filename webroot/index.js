09bcd35f224f64222e8f42bfad3f74eefd35545f
François Fouquet <francois.fouquet@datathings.com>
2025-01-16 12:03:44 +0100

    cleanup

--- webroot/index.js
+++ webroot/index.js
@@ -1,6 +1,6 @@
 await gc.sdk.init();
-var filters_el = /** @type {any}*/ (document.getElementById("filters"));
-var results_el = /** @type {any}*/ (document.getElementById("results"));
+var filters_el = /** @type {gc.GuiInputObject}*/ (document.getElementById("filters"));
+var results_el = /** @type {gc.GuiTable}*/ (document.getElementById("results"));
 var details_el = /** @type {any}*/ (document.getElementById("details"));
 const filters = new gc.Filters(0,1000, "default");
 filters_el.value = filters;
@@ -8,8 +8,10 @@ filters_el.addEventListener('gui-change', async function(){
     results_el.value = await gc.fake_table(filters);
 });
 results_el.value = await gc.fake_table(filters);
+// @ts-ignore
 results_el.addEventListener('gui-click', async function(/** @type{any} */ev){
-    var line = /** @type{gc.TableLine} */ (results_el.value[ev.detail.rowIdx]);
+    // @ts-ignore
+    var line =  (results_el.value)[ev.detail.rowIdx];
     var obj_el = document.createElement('gui-object');
     obj_el.value = line;
     obj_el.header = true;
